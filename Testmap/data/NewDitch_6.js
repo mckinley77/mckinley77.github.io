c6610dbb760da7cde2fa3b8427d526fb9b08eea9
Alastair McKinley <amckinley03@googlemail.com>
2024-04-13 13:59:12 +0100

    Test

---
+++ Testmap/data/NewDitch_6.js
@@ -0,0 +1,12 @@
+var json_NewDitch_6 = {
+"type": "FeatureCollection",
+"name": "NewDitch_6",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -5.986051104742691, 54.545648842829152 ], [ -5.985663987151112, 54.545492852348467 ], [ -5.985380533830612, 54.54536692866504 ], [ -5.985188101565748, 54.545273037147766 ] ] ] } },
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -5.98610701277156, 54.546030218384679 ], [ -5.986656040387358, 54.545994012874552 ] ] ] } },
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -5.983926376098425, 54.543561004017747 ], [ -5.984236932932084, 54.543184547640692 ], [ -5.984440176635186, 54.543103826718259 ], [ -5.984843641145047, 54.543111861332697 ] ] ] } },
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -5.98300583299916, 54.544362411814127 ], [ -5.983208910573929, 54.544303335556727 ] ] ] } },
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -5.989171128713919, 54.550412721236825 ], [ -5.989145015986772, 54.550612519619293 ], [ -5.989119193609957, 54.550774497718365 ], [ -5.9889036686445, 54.550948785860648 ], [ -5.988739555035064, 54.551221537254399 ], [ -5.988649608426774, 54.551372874242475 ] ] ] } }
+]
+}
