6a0efd1af5e75b8c139322ff2722b76b496c30e7
mckinley77 <99842964+mckinley77@users.noreply.github.com>
2024-04-13 14:46:20 +0100

    Add files via upload

---
+++ Dunmurry 13 April/data/LakeDrain_3.js
@@ -0,0 +1,10 @@
+var json_LakeDrain_3 = {
+"type": "FeatureCollection",
+"name": "LakeDrain_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -5.98761214534603, 54.548989115129004 ], [ -5.987639255364451, 54.548869136569905 ], [ -5.987609217066627, 54.548751663199219 ], [ -5.987487479185199, 54.548594396238684 ] ] ] } },
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -5.987449446776671, 54.548484401875299 ], [ -5.98710676100526, 54.548222867659696 ], [ -5.986551105592467, 54.547909957891747 ], [ -5.986155414760124, 54.547642226695544 ] ] ] } },
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -5.987449180835053, 54.548482638791739 ], [ -5.987485294467829, 54.548590065666332 ] ] ] } }
+]
+}
